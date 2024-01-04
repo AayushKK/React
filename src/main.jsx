4ac60be78773b9e6d10d6324c0cd50ce9117f2df
Aayush Kharel <10891kharelaayush@gmail.com>
2024-01-04 23:18:09 +0545

    added image slider

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ImageSlider from './ImageSlider.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ImageSlider />
     <App />
+   
   </React.StrictMode>,
 )
 
